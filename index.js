f663b9cac550e279d088456108b75cc352b7ff46
Rituchoudhary67 <114747083+Rituchoudhary67@users.noreply.github.com>
2022-11-18 20:19:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const table = document.querySelector("table");
+const tdArr = [];
+
+const handleClick =(e) => {
+    if(e.target.classList.contains("table-col")){
+        if(tdArr.length > 1){
+            let rCol= tdArr.shift();
+            rCol.classList.remove("hero-col");
+        }
+        tdArr.push(e.target);
+    }
+    tdArr.forEach((el) => {
+        el.classList.add("hero-col");
+        console.log(el);
+    })
+}
+table.addEventListener('click',handleClick)
+
+
+
+
+
